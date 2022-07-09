82b32d43c488a287db254675b7543b3b970d5fd0
eduardoboos <eduardoboos@yahoo.com.br>
2022-07-08 21:58:57 -0300

    ajustando o codigo

--- src/api/views.py
+++ src/api/views.py
@@ -1,11 +1,14 @@
 
 
 # Create your views here.
+
 import email
+from itertools import count
 import json
 from django.http import JsonResponse
-from .models import Pessoa,Funcionario
-from .serializers import FuncionarioSerializer, PessoaSerializer
+from .models import Funcionario,Perfil,Curriculo
+from .serializers import FuncionarioSerializer,PerfilSerializer, CurriculoSerializer
+from src.api import serializers
 
 def health(request):
     return JsonResponse({'mensagem': 'ok'} ,safe=False)
@@ -70,14 +73,92 @@ def funcionario_info(request):
 
         return JsonResponse({"mensagem": 'usuario não encontrado'})    
 
-    return JsonResponse({"mensagem": "metodo invalido"})          
+    return JsonResponse({"mensagem": "metodo invalido"})         
+
+
+def perfil_info(request):
+    """View da model Perfil."""
+
+    query_params = request.GET
+
+    if request.method == 'GET':
+        lista_perfis = Perfil.objects.all().values()
+        serializer = PerfilSerializer(lista_perfis, many=True)
+
+        return JsonResponse({'Perfil': serializer.data})
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        perfil = perfil()
+        perfil.nome = data.get('nome')
+        perfil.sobrenome = data.get('sobrenome')
+        perfil.avatar = data.get('avatar')
+        perfil.email = data.get('email')
+        perfil.perfil = data.get('perfil')
+        perfil.github = data.get('github')
+        perfil.celular = data.get('celular')
+
+        existe_perfil = Perfil.objects.filter(email=perfil.email).count()
+        if existe_perfil:
+           return JsonResponse({'mensagem': 'o usuario já existe'})
+
+        perfil.save()
+        serializer = PerfilSerializer(Perfil, many=False)
+        return JsonResponse({'objeto': serializer.data})   
+
+
+    if request.method == 'PUT':
+       data = json.loads(request.body) 
+       existe_perfil = Perfil.objects.filter(email=query_params.get('email')).count()
+
+       if existe_perfil:
+          perfil.nome = data.get('nome')
+          perfil.sobrenome = data.get('sobrenome')
+          perfil.avatar = data.get('avatar')
+          perfil.email = data.get('email')
+          perfil.perfil = data.get('perfil')
+          perfil.github = data.get('github')
+          perfil.celular = data.get('celular')
+
+          perfil.save()
+          serializer = PerfilSerializer(Perfil, many=False)
+          return JsonResponse({'objeto': serializer.data})
+
+    return JsonResponse({'mensagem': 'email nao existe'})   
+
+
+    if request.method == 'DELETE':
+        existe_email -query-params.get('email')
+
+        perfil = perfil.object.filter(email=existe_email).first()
+
+        if perfil:
+           Perfil.delete()
+           return JsonResponse({'mensagem': 'objeto deletado'})
+
+           return JsonResponse({'mensagem': 'objeto nao encontrado'})
 
 
 
 
+def curriculo_info(request):
+    """View da model Perfil."""
 
+    query_params = request.GET
 
+    if request.method == 'GET':
+        return JsonResponse({'mensagem': 'curriculo OK'})
 
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+    if request.method == 'PUT':
+       data = json.loads(request.body) 
+
+    if request.method == 'DELETE':
+       data = json.loads(request.body)    
+        
 
 
 
